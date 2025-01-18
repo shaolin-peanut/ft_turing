01671ba2b4ad5687d6f54e2842dcfa1507c17b48
Marie Chalard <marieamelie.1966@gmail.com>
2025-01-18 15:47:10 +0100

    feat: clean main

--- srcs/main.ml
+++ srcs/main.ml
@@ -14,8 +14,6 @@ let print_all_transitions transitions_list =
 
 let () =
   let machine_name = "machines/unary_sub.json" in
-  (* let json = Yojson.Basic.from_file ("machines/" ^ machine_name ^ ".json") in *)
-  (* print_endline (Yojson.Basic.pretty_to_string json); *)
   let machine = Parser.parse_json machine_name;
   in print_endline machine.name;
   print_endline (String.concat ", " machine.alphabet);
